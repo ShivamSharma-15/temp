25ee2bc6351e081e99879cee50f6e6d967a8e90a
Shivam <main.shivamsharma@gmail.com>
2025-12-04 12:08:07 +0530

    Endpoint set

--- src/store/useFleetOverviewStore.js
+++ src/store/useFleetOverviewStore.js
@@ -5,31 +5,31 @@ export const useFleetOverviewStore = create((set) => ({
   loading: false,
   error: null,
   data: [
-      {
-        site_id: "id123",
-        site_data: {
-          name: "Site A",
-          site_capacity: 5375,
-          generation: 14676.3,
-          netExport: 1200,
-          cufGen: 12,
-          cufExport: 11,
-          tlLoss: 9
-        }
+    {
+      site_id: "id123",
+      site_data: {
+        name: "Site A",
+        site_capacity: 5375,
+        generation: 14676.3,
+        netExport: 1200,
+        cufGen: 12,
+        cufExport: 11,
+        tlLoss: 9,
       },
-      {
-        site_id: "id124",
-        site_data: {
-          name: "Site B",
-          site_capacity: 3000,
-          generation: 9940,
-          netExport: 680,
-          cufGen: 10,
-          cufExport: 9,
-          tlLoss: 8
-        }
-      }
-    ],
+    },
+    {
+      site_id: "id124",
+      site_data: {
+        name: "Site B",
+        site_capacity: 3000,
+        generation: 9940,
+        netExport: 680,
+        cufGen: 10,
+        cufExport: 9,
+        tlLoss: 8,
+      },
+    },
+  ],
 
   // Default Range - Filled later by component
   range: { from: null, to: null },
@@ -41,15 +41,19 @@ export const useFleetOverviewStore = create((set) => ({
     set({ loading: true, error: null });
 
     try {
-      const formattedFrom = Math.floor(from.getTime() / 1000);;
-      const formattedTo = Math.floor(to.getTime() / 1000);;
-      
+      const formattedFrom = Math.floor(from.getTime() / 1000);
+      const formattedTo = Math.floor(to.getTime() / 1000);
 
-      console.log(`${import.meta.env.VITE_API_URL}/api/overview-data/v1?from=${formattedFrom}&to=${formattedTo}`);
-
+      console.log(
+        `${
+          import.meta.env.VITE_API_URL
+        }/api/overview-data/v1?from=${formattedFrom}&to=${formattedTo}`
+      );
 
       const res = await fetch(
-        `/api/fleet-overview?from=${formattedFrom}&to=${formattedTo}`
+        `${
+          import.meta.env.VITE_API_URL
+        }/api/integrate/overview-data/v1?from=${formattedFrom}&to=${formattedTo}`
       );
 
       const json = await res.json();
